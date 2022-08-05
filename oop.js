279ace7e2e536863e1300bbc10afefcb9c9fbee4
Timothy Pendarvis <trpendarvis@gmail.com>
2022-08-05 13:29:33 -0600

    Complete challenge 1 and 2, start 3

--- oop.js
+++ oop.js
@@ -1,18 +1,46 @@
 // Write a class called Unicorn
 // it should have a dynamic name property
 // it should have a color property, that is silver by default
-// it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+// it should have a method called "say" that returns whatever string is passed in,
+// with "*~*" at the beginning and end of the string
+class Unicorn {
+  constructor(name) {
+    this.name = name;
+    this.color = "silver";
+  }
 
+  say(text) {
+    console.log("*~*" + text + "*~*")
+  }
+}
 
+var jesseTheUnicorn = new Unicorn("Steve");
+
+console.log(jesseTheUnicorn);
+
+jesseTheUnicorn.say("Hey folks");
 
 // Write a class called Vampire
 // it should have a dynamic name property
 // it should have a pet property, that is a bat, by default BUT it could be dynamic if info is passed in initially
 // it should have a thirsty property, that is true by default
 // it should have a drink method. When called, the thirsty property changes to false
-
-
-
+class Dragon {
+  constructor(name, color, isHungry, timesFed) {
+    this.name = name;
+    this.color = color;
+    this.isHungry = isHungry;
+    this.timesFed = timesFed;
+    }
+    feedDragon() {
+    this.timesFed++;
+     if (this.timesFed === 4) {
+     this.isHungry = false
+  }
+}
+}
+var Dragon1 = new Dragon("Draco", "blue", true, 0);
+console.log(Dragon1);
 //  Write a Dragon class
 // it should have a dynamic name property (string)
 //  it should have a dynamic rider property (string)
@@ -21,7 +49,30 @@
 //  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
   // HINT: This requires you to use your knowledge from the conditionals lesson
 
+  class Dragon {
+    constructor(name, pet) {
+      this.name = name;
+      this.rider = pet;
+      this.color = color;
+      this.isHungry = true;
+    }
+    changeName(newName) {
+      this.name = newName
+    changeColor(newColor) {
+      this.color = newColor
+    changeRider(newRider) {
+      this.Rider = newRider
+    }
+    }
+    drink() {
+      this.thirsty = false
 
+    }
+  }
+  var Dragon = new Dragon("name");
+  console.log(Dragon);
+  Dragon.("");
+  Dragon.drink()
 
 //  BIG CHALLNEGE: Write a Hobbit class
 //  it should have a dynamic name property (string)
